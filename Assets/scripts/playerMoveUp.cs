322b17b6fc2798cc0eaf2bffd1461432fa2ff165
dmj0shua1 <dmj0shua1@gmail.com>
2016-07-23 16:44:00 +0800

    Movement
    it can now go up , but character goes out of place

---
+++ Assets/scripts/playerMoveUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMoveUp : MonoBehaviour {
+    public GameObject protag;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider moveUp)
+    {
+        if (moveUp.gameObject.CompareTag("Player"))
+        {
+            SwipeMovement.moveUp = true;
+
+        }
+    }
+}
