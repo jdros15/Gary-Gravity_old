d8e92ea873e8b4662f17149faccef14546f51857
dmj0shua1 <dmj0shua1@gmail.com>
2016-07-27 13:47:14 +0800

    Pause
    optimized pausing

---
+++ Assets/scripts/pauseScale.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+    public float timeScale;
+	Camera cam;
+	//private Blur Blurscript;
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Pause(){
+		Time.timeScale = 0f;
+		cam.SendMessage ("TurnBlurOn");
+	
+
+	}
+}
