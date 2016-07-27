c66596bd0708ecb4b185d3bf3c3a55920a75c797
dmj0shua1 <dmj0shua1@gmail.com>
2016-07-27 13:29:30 +0800

    music
    gary gravity's music is now playing flawlessly

---
+++ Assets/scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    public GameObject intro,loop;
+    private AudioSource introMusic,loopMusic;
+	// Use this for initialization
+	void Start () {
+       introMusic = intro.GetComponent<AudioSource>();
+       loopMusic = loop.GetComponent<AudioSource>();
+        if (!loopMusic.isPlaying)
+        {
+            introMusic.Play();
+            Invoke("PlayMusic", 12);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+	}
+    void PlayMusic()
+    {
+        loopMusic.Play();
+    }
+}
