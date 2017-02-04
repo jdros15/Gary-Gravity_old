4eff9ecfe3cfb284c370882639452b531eda3079
dmj0shua1 <dmj0shua1@gmail.com>
2017-02-04 13:59:09 +0800

    Music loop bug fix

--- Assets/scripts/MusicPlayer.cs
+++ Assets/scripts/MusicPlayer.cs
@@ -4,7 +4,22 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
     public GameObject intro,loop;
     private AudioSource introMusic,loopMusic;
-    
+    private static MusicPlayer instance = null;
+    public static MusicPlayer Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+         if (instance != null && instance != this) {
+         Destroy(this.gameObject);
+         return;
+     } else {
+         instance = this;
+     }
+     DontDestroyOnLoad(this.gameObject);
+    }
 
 	// Use this for initialization
 	void Start () {
