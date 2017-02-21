00eb2b232dff2fc600f7fac43771a53bdfd83b1a
dmj0shua1 <dmj0shua1@gmail.com>
2017-02-21 09:10:17 +0800

    Unity UI Extensions and New scrolling component

---
+++ Assets/scripts/animationBoolChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationBoolChanger : MonoBehaviour {
+
+    public GameObject objectWithAnimator;
+    Animator anim;
+    public string parameterName;
+    public bool conditionToSetForParameter;
+	// Use this for initialization
+	void Start () {
+     
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeBoolean()
+    {
+        anim = objectWithAnimator.GetComponent<Animator>();
+        anim.SetBool(parameterName, conditionToSetForParameter);
+    }
+}
