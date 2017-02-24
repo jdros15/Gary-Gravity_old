410c553b4d85dbeffda8de1904b68a1ea94278c7
dmj0shua1 <dmj0shua1@gmail.com>
2017-02-24 12:54:16 +0800

    another error commit

--- Assets/scripts/RateApp.cs
+++ Assets/scripts/RateApp.cs
@@ -14,11 +14,6 @@ public class RateApp : MonoBehaviour {
 	}
 
     public void rAppbeh(){
-
-#if UNITY_ANDROID
         Application.OpenURL("https://play.google.com/store/apps/details?id=com.loopbook.garyg");
-#elif UNITY_IPHONE
-        Application.OpenURL("http://itunes.apple.com/app/id1068341133");
-#endif
     }
 }
